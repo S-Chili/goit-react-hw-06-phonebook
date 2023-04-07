2c1a465aef3a340f6dfb20f222bd47d999096bdb
Anastasiia <schili@outlook.com>
2023-04-07 17:11:53 +0300

    got rid of the props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,9 @@
-import { useState, useEffect } from 'react';
 import ContactForm from './Contacts/ContactForm';
 import Contacts from './Contacts/Contacts';
-import Filter from './Contacts/Filter';
 import Wrapper from './Contacts/Wrapper';
-import { useSelector } from 'react-redux';
 
 
 export default function App() {
-  const filter = useSelector((state) => state.contacts.filter);
-  const contacts = useSelector((state) => state.contacts.contacts);
-
-  const [filteredContacts, setFilteredContacts] = useState([]);
-
-  useEffect(() => {
-    setFilteredContacts(
-      contacts.filter((contact) =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-      )
-    );
-  }, [contacts, filter]);
 
   return (
     <div>
@@ -28,8 +13,8 @@ export default function App() {
       </Wrapper>
       <Wrapper>
         <h2>Contacts</h2>
-        <Filter filter={filter} />
-        <Contacts contacts={filteredContacts}/>
+        
+        <Contacts/>
       </Wrapper>
     </div>
          
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import css from './Contacts.module.css'
-import {  useDispatch } from 'react-redux';
+import Filter from './Filter';
+import {  useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice'
 
-function Contacts({contacts}) {
+function Contacts() {
   const dispatch = useDispatch();
 
+  const filter = useSelector((state) => state.contacts.filter);
+  const contacts = useSelector((state) => state.contacts.contacts);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-      <div>
-           <ul>{contacts.map((contact) => (
-                <li key={contact.id} className={css.ContactListItem}>
-                <p>{contact.name}</p>
-                <p className={css.TitlePhoneNumber}>{contact.number}</p>
-                   <button onClick={() => dispatch(deleteContact(contact.id))} className={css.FormBtn}>Delete</button>
-                </li>
+   <div>
+      <Filter filter={filter} />
+         <ul>{filteredContacts.map((contact) => (
+               <li key={contact.id} className={css.ContactListItem}>
+               <p>{contact.name}</p>
+               <p className={css.TitlePhoneNumber}>{contact.number}</p>
+                  <button onClick={() => dispatch(deleteContact(contact.id))} className={css.FormBtn}>Delete</button>
+               </li>
             ))}
-           </ul>
-        </div>
+         </ul>
+   </div>     
   );
 }
 
